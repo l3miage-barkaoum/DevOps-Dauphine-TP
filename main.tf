556f876bfea9a077b49199304fd24fad376d5c94
l3miage-barkaoum <meriam.barkaoui@etu.univ-grenoble-alpes.fr>
2023-10-05 09:32:05 +0000

    Partie 3 finie

--- main.tf
+++ main.tf
@@ -35,9 +35,12 @@ resource "google_project_service" "sqladmin" {
 resource "google_project_service" "cloudbuild" {
   service = "cloudbuild.googleapis.com"
 }
+resource "google_project_service" "cloud_run" {
+  service = "run.googleapis.com"
+}
  # Pour le artifact_registry
 resource "google_artifact_registry_repository" "my-repo" {
-  location      = "us-west1"
+  location      = "us-central1"
   repository_id = "website-tools"
   description   = "Repo Docker"
   format        = "DOCKER"
@@ -55,4 +58,46 @@ resource "google_sql_user" "wordpress" {
 resource "google_sql_database" "wordpress" {
   name     = "wordpress"
   instance = "main-instance"
+}
+resource "google_cloud_run_service" "default" {
+name     = "serveur-wordpress"
+location = "us-central1"
+
+template {
+   spec {
+      containers {
+      image = "us-central1-docker.pkg.dev/solid-course-399708/website-tools/mon-image-wordpress"
+      ports {
+        container_port= 80
+      }
+      }
+   }
+
+   metadata {
+      annotations = {
+            "run.googleapis.com/cloudsql-instances" = "solid-course-399708:us-central1:main-instance"
+      }
+   }
+}
+
+traffic {
+   percent         = 100
+   latest_revision = true
+}
+}
+data "google_iam_policy" "noauth" {
+   binding {
+      role = "roles/run.invoker"
+      members = [
+         "allUsers",
+      ]
+   }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_service.default.location
+   project     = google_cloud_run_service.default.project
+   service     = google_cloud_run_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
 }
